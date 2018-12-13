65046899cf4c3367180f0f968f2de8599729115d
soyoungna <hos05360@naver.com>
2018-12-13 20:29:17 +0900

    라스트!!!!

--- main.c
+++ main.c
@@ -18,14 +18,12 @@ int main(int argc, char *argv[]) {
 	void *arg; 
 	int cnt; 
 	 
-    fp=fopen("movie.dat","r");
+    fp=fopen("movie.dat","r");//파일 열기 
 	
+	list = list_genList();//liked list를 만든다고 선언 
 	
-	list = list_genList();
-	
-	 
-
-	while (fscanf(fp,"%s %s %d %f ", name, country, &runTime, &score) != EOF )
+	while (fscanf(fp,"%s %s %d %f ", name, country, &runTime, &score) != EOF )//파일을 읽기 문자열을 name,country,runTime,score로 받아 놓기 그리고 node하나 추가
+	  
 	{	
 	 
 	     mvInfo = mv_genMvInfo(name,score,runTime,country);
@@ -37,18 +35,18 @@ int main(int argc, char *argv[]) {
 
 	fclose(fp);
 	
-	printf("reading done ! find %i movies \n\n\n",list_len(list));
+	printf("\n reading done! find %i movies! \n\n\n",list_len(list));
 	
-	while(exit_flag == 0)
+	while(exit_flag == 0)//main menu 만들기  
 	{
-	    printf("-------------MENU-------------\n");
+	    printf("----------------MENU----------------\n");
 		printf("1.print all the movies\n");
 		printf("2.search for specific country movies\n");
 		printf("3.search for specific runTime movies\n");	
 		printf("4.search for specific score movies\n");
 		printf("5.exit\n");
 		
-		printf("select an option : ");
+		printf("select an option : ");//option 1,2,3,4,5중에서 받기 switch함수로 넘어간다. 
 		scanf("%d",&option);
 		
 		switch(option)
@@ -56,40 +54,41 @@ int main(int argc, char *argv[]) {
 			case 1: 
 				printf("\nprinting all the movies in the list.....\n\n\n");
 				printf("----------------------------------------\n");
-				repFunc = mv_printAll;
+				repFunc = mv_printAll; //repFunc 포인터가 mv_printAll함수와 같다. 
 				arg = NULL;
-				list_repeatFunc(repFunc,arg,list);
+				list_repeatFunc(repFunc,arg,list);//위 함수들 실행.  
 				break;
 				
 			case 2: 
-                printf("input a country : ");
+                printf("input a country : ");//country받기  
                 scanf("%s",country);
                 printf("----------------------------------------\n");
                 
                 repFunc = mv_printCountry;
-                arg=country;
+                arg=country;//내가 쓴 country가 arg 포인터로 넘어감.  
                 list_repeatFunc(repFunc,arg,list);
+                
 				break;
 				
 			case 3: 
 
-                printf("input a runTime : ");
+                printf("input a runTime : ");//runTime 받기,내가쓴 runTime보다 큰 영화들이 출력될 것이다. 
                 scanf("%i",&runTime);
                 
                 repFunc = mv_printRunTime;
                 arg = &runTime;
                 list_repeatFunc(repFunc,arg,list);
-                
 
 				break;
 				
 			case 4: 
-				printf("input a score : ");
+				printf("input a score : ");//score받기, 내가 입력한 score보다 높은 영화들이 출력된다.  
                 scanf("%f",&score);
                 
 				repFunc = mv_printScore;
 				arg = &score;
 				list_repeatFunc(repFunc,arg,list);
+				
 				break;
 				
 			case 5:
--- movie.c
+++ movie.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "movie.h"
 
-//structure definition
+
 typedef struct movInfo {
 	char name[100];
 	float score;
@@ -11,22 +11,22 @@ typedef struct movInfo {
 	char madeIn[10];
 } movInfo_t;
 
-void* mv_genMvInfo(char* name, float score, int runTime, char* country)
+void* mv_genMvInfo(char* name, float score, int runTime, char* country)//main함수에거 받은 정보를 구조체 안에 넣기 
 {
-	movInfo_t* mvPtr;
+	movInfo_t* mvPtr; //구조체 movInfo_t를 포인터 mvPtr로 선언해줌. 
 	
-	mvPtr = (movInfo_t*) malloc(sizeof(movInfo_t));
+	mvPtr = (movInfo_t*) malloc(sizeof(movInfo_t));//동적메모리 선언 
 	
-	strcpy(mvPtr->name,name);
+	strcpy(mvPtr->name,name);//문자열 복사 구조체의 name이 내가 받은 name과 같다 
 	strcpy(mvPtr->madeIn,country);
-	mvPtr->runTime=runTime;
+	mvPtr->runTime=runTime;//숫자들은 문자열 복사 함수를 쓸 필요없음. 
 	mvPtr->score=score;
-	//allocate memory and set the member variables
+
 	
 	return (void*)mvPtr;
 }
 
-void printMv(void* obj)
+void printMv(void* obj)//영화들의 정보를 출력하는 함수 
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	if (mvPtr == NULL)
@@ -36,30 +36,31 @@ void printMv(void* obj)
 	
 	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
 	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	printf("\n");
 	
 	return;
 }
 
-int mv_printAll(void* obj, void* arg)
+int mv_printAll(void* obj, void* arg)//모든 영화정보를 출력하는 함수. 
 {
 	printMv(obj);
 	
 	return;
 }
 
-int mv_printScore(void* obj, void* arg)
+int mv_printScore(void* obj, void* arg)//내가 입력한 점수보다 높은 점수들의 영화를 출력하는 함수.  
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	
    if((*(float*)arg)<=mvPtr->score){
    	
    	printMv(obj);//void 포인터를 float포인터로 형변환하였다. 
-   	
+   	printf("\n");
    }
    return;
 }
 
-int mv_printRunTime(void* obj, void* arg)
+int mv_printRunTime(void* obj, void* arg)//내가 입력한 runtime보다 긴 영화들을 출력하는 함수  
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	
@@ -71,7 +72,7 @@ int mv_printRunTime(void* obj, void* arg)
    return;
 }
 
-int mv_printCountry(void* obj, void* arg)
+int mv_printCountry(void* obj, void* arg)//내가 입력한 특정 나라의 영화들을 출력하는 함수  
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
 	
